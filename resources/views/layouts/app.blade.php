0dd480c0d4e4df7a8ecfed0f10def0dab836ba10
WalterG16 <walter@DESKTOP-R59EOBS.localdomain>
2024-05-13 19:40:14 -0300

    refactory code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,77 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @stack('style')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{asset('js/app.js')}}" defer></script>
-    
+
     <title>MyPhotoBook - @yield('titulo')</title>
 
+    @livewireStyles
 </head>
-    <body class="bg-gray-100">
 
-        <header class="p-5 border-b bg-white shadow">
+<body class="bg-gray-100">
 
-            <div class="container mx-auto flex justify-between items-center">
+    <header class="p-5 border-b bg-white shadow">
 
-                <a href="{{ route('home') }}" class="text-3xl font-black">
-                    MyPhotoBook
+        <div class="container mx-auto flex justify-between items-center">
+
+            <a href="{{ route('home') }}" class="text-3xl font-black">
+                MyPhotoBook
+            </a>
+
+            @auth
+            <nav class="flex gap-2 items-center">
+                <a class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded text-sm uppercase font-bold cursor-pointer"
+                    href=" {{ route('post.create') }} ">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                    Crear
                 </a>
 
-                @auth
-                    <nav class="flex gap-2 items-center">
-                        <a class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded text-sm uppercase font-bold cursor-pointer" href=" {{ route('post.create') }} ">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            Crear
-                        </a>
+                <a class="font-bold  text-gray-600" href=" {{ route('posts.index', auth()->user()->username ) }} ">
+                    <span class="font-normal">
+                        {{ auth()->user()->username }}
+                    </span>
+                </a>
 
-                        <a class="font-bold  text-gray-600" href=" {{ route('posts.index', auth()->user()->username ) }} ">
-                            <span class="font-normal">
-                                {{ auth()->user()->username }}
-                            </span>
-                        </a>
+                <form method="POST" action=" {{ route('logout') }} ">
+                    @csrf
+                    <button type="submit" class="font-bold uppercase text-gray-600">
+                        cerrar sesion
+                    </button>
+                </form>
+            </nav>
+            @endauth
 
-                        <form method="POST" action=" {{ route('logout') }} ">
-                            @csrf
-                            <button type="submit" class="font-bold uppercase text-gray-600">
-                                cerrar sesion
-                            </button>
-                        </form>
-                    </nav>
-                @endauth
+            @guest
+            <nav class="flex gap-2 items-center">
+                <a class="font-bold uppercase text-gray-600" href="{{ route('login') }}">Registrarse</a>
+                <a class="font-bold uppercase text-gray-600" href="{{ route('register') }}">Crear cuenta</a>
+            </nav>
+            @endguest
 
-                @guest
-                    <nav class="flex gap-2 items-center">
-                        <a class="font-bold uppercase text-gray-600" href="{{ route('login') }}">Registrarse</a>
-                        <a class="font-bold uppercase text-gray-600" href="{{ route('register') }}">Crear cuenta</a>
-                    </nav>
-                @endguest
+        </div>
 
-            </div>
+    </header>
 
-        </header>
+    <main class="container mx-auto mt-10">
 
-        <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">
 
-            <h2 class="font-black text-center text-3xl mb-10">
+            @yield('titulo')
 
-                @yield('titulo')
+        </h2>
 
-            </h2>
+        @yield('contenido')
 
-            @yield('contenido')
+    </main>
 
-        </main>
+    <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
+        MyPhotoBook - TODOS LOS DERECHOS RESERVADOS {{ now()->year }}
 
-            MyPhotoBook - TODOS LOS DERECHOS RESERVADOS {{ now()->year }}
+    </footer>
 
-        </footer>
+    @livewireScripts
+</body>
 
-    </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,155 +1,118 @@
 @extends('layouts.app')
 
 @section('titulo')
-    
-    {{ $post->titulo }}
+
+{{ $post->titulo }}
 
 @endsection
 
 @section('contenido')
-    
-    <div class="container mx-auto md:flex">
 
-        <div class="md:w-1/2">
+<div class="container mx-auto md:flex">
 
-            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+    <div class="md:w-1/2">
 
-            <div class="p-3 flex items-center gap-4">
+        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
 
-                @auth
-
-                    @if($post->checkLike( auth()->user() ))
-
-                        <form method="POST" action="{{ route('posts.like.destroy', $post) }}">
-                            @method('DELETE')
-                            @csrf
-                            <div class="my-4">
-                                <button type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
-                    
-                    @else
-
-                        <form method="POST" action="{{ route('posts.like.store', $post) }}">
-                            @csrf
-                            <div class="my-4">
-                                <button type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
-
-                    @endif
-                    
-                    
-                @endauth
-
-                <p class="font-bold "> {{ $post->likes->count() }} 
-                    <span class="font-normal"> LIKES </span>
-                </p>
-            </div>
-
-            <div>
-                <p class="font-bold"> {{ $post->user->username }}</p>
-                <p class="text-sm text-gray-500"> {{ $post->created_at->diffForHumans() }} </p>
-                <p class="mt-5"> {{ $post->descripcion }} </p>
-            </div>
+        <div class="p-3 flex items-center gap-4">
 
             @auth
-                @if ($post->user_id == auth()->user()->id)
-                    <form method="POST" action="{{ route('post.destroy', $post) }}">
-                        @method('DELETE')
-                        @csrf
-                        <input 
-                            type="submit"
-                            value="Eliminar Publicacion"
-                            class="bg-red-500 hover:bg-red-800 rounded text-white font-bold mt-4 cursor-pointer px-2 py-1"
-                        />
-                    </form>
-                @endif
-                
-            @endauth
-            
 
+            @php
+            $mensaje = "alojaaaaa";
+            @endphp
+
+            <livewire:like-post :post="$post" />
+
+            @endauth
+
+            
         </div>
 
-        <div class="md:w-1/2 p-5">
-            
-            <div class="shadow bg-white p-5 mb-5">
+        <div>
+            <p class="font-bold"> {{ $post->user->username }}</p>
+            <p class="text-sm text-gray-500"> {{ $post->created_at->diffForHumans() }} </p>
+            <p class="mt-5"> {{ $post->descripcion }} </p>
+        </div>
 
-                @auth
-                    <p class="text-xl font-bold text-center mb-4">Introduce tu comentario</p>
+        @auth
+        @if ($post->user_id == auth()->user()->id)
+        <form method="POST" action="{{ route('post.destroy', $post) }}">
+            @method('DELETE')
+            @csrf
+            <input type="submit" value="Eliminar Publicacion"
+                class="bg-red-500 hover:bg-red-800 rounded text-white font-bold mt-4 cursor-pointer px-2 py-1" />
+        </form>
+        @endif
 
-                    @if (session('mensaje'))
-                        
-                        <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
-                            {{session('mensaje')}}
-                        </div>
+        @endauth
 
-                    @endif
 
-                    <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user]) }}" method="POST">
-                        @csrf
-                        <div class="mb-5">
-                            <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
-                                Agrega un Comentario
-                            </label>
-        
-                            <textarea
-                                    type="text" 
-                                    name="comentario" 
-                                    id="comentario" 
-                                    placeholder="Agrega un comentario"
-                                    class="resize-none border p-3 w-full rounded-lg @error('comentario') border-red-500 @enderror"
-                            ></textarea>
-        
-                            @error('descripcion')
-                                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
-                            @enderror
-        
-                        </div>
+    </div>
 
-                        <input 
-                            type="submit" 
-                            value="Comentar" 
-                            class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" 
-                        >
+    <div class="md:w-1/2 p-5">
 
-                    </form>
-                @endauth
+        <div class="shadow bg-white p-5 mb-5">
 
-                <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
+            @auth
+            <p class="text-xl font-bold text-center mb-4">Introduce tu comentario</p>
 
-                    @if ($post->comentarios->count())
+            @if (session('mensaje'))
 
-                        @foreach ($post->comentarios as $comentario)
-                            <div class="p-5 border-gray-300 border-b ">
+            <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                {{session('mensaje')}}
+            </div>
 
-                                <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold">
-                                    {{ $comentario->user->username }}
-                                </a>
-                                <p class="w-50"> {{ $comentario->comentario }} </p>
-                                <p class="text-sm text-gray-500"> {{ $comentario->created_at->diffForHumans() }} </p>
- 
-                            </div>
-                        @endforeach
-                    
-                    @else
-                        <p class="p-10 text-center"> NO HAY COMENTARIOS </p>
-                    @endif
+            @endif
+
+            <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user]) }}" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Agrega un Comentario
+                    </label>
+
+                    <textarea type="text" name="comentario" id="comentario" placeholder="Agrega un comentario"
+                        class="resize-none border p-3 w-full rounded-lg @error('comentario') border-red-500 @enderror"></textarea>
+
+                    @error('descripcion')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
+                    @enderror
 
                 </div>
+
+                <input type="submit" value="Comentar"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+
+            </form>
+            @endauth
+
+            <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
+
+                @if ($post->comentarios->count())
+
+                @foreach ($post->comentarios as $comentario)
+                <div class="p-5 border-gray-300 border-b ">
+
+                    <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold">
+                        {{ $comentario->user->username }}
+                    </a>
+                    <p class="w-50"> {{ $comentario->comentario }} </p>
+                    <p class="text-sm text-gray-500"> {{ $comentario->created_at->diffForHumans() }} </p>
+
+                </div>
+                @endforeach
+
+                @else
+                <p class="p-10 text-center"> NO HAY COMENTARIOS </p>
+                @endif
 
             </div>
 
         </div>
 
     </div>
+
+</div>
 
 @endsection
